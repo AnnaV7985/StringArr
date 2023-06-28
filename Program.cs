02ca9cc576a2822eb9afd0a7245456bde10f395e
Anna V <lianna2015new@yandex.ru>
2023-06-28 11:42:50 +1000

    add 2,3 string

--- Program.cs
+++ Program.cs
@@ -1,4 +1,8 @@
 ﻿string array = "Hello, 2, world, :-)";
 string[] simbol1 = array.Split(' ');
 Console.WriteLine ($"Первый массив строк [{String.Join (" " , simbol1)}]");
+string[] simbol2 = new string [4]{"1234", "1567", "-2", "computer science"};
+Console.WriteLine ($"Второй массив строк [{String.Join (", " , simbol2)}]");
+string[] simbol3 = new string [3]{"Russia", "Denmark", "Kazan"};
+Console.WriteLine ($"Третий массив строк [{String.Join (", " , simbol3)}]");
 
